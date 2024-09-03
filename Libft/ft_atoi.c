26dce57fb96de53c8b83a5422954462fafef481b
nastamid <nikoloz.astamidze@gmail.com>
2024-09-03 17:04:55 +0200

    added Libft

---
+++ Libft/ft_atoi.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nastamid <nastamid@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/22 23:21:52 by nastamid          #+#    #+#             */
+/*   Updated: 2024/07/23 15:11:33 by nastamid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	whitespaces(char *str, int *ptr_i)
+{
+	int	count;
+	int	i;
+
+	i = 0;
+	count = 1;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	while (str[i] && (str[i] == '+' || str[i] == '-'))
+	{
+		if (str[i] == '-')
+			count *= -1;
+		i++;
+	}
+	*ptr_i = i;
+	return (count);
+}
+
+int	ft_atoi(char *str)
+{
+	int	sign;
+	int	result;
+	int	i;
+
+	result = 0;
+	sign = whitespaces(str, &i);
+	while (str[i] && str[i] >= '0' && str[i] <= '9')
+	{
+		result *= 10;
+		result += str[i] - 48;
+		i++;
+	}
+	result *= sign;
+	return (result);
+}
+
+// #include <stdio.h>
+
+// int	main(int argc, char *argv[])
+// {
+// 	printf("%d argc: %d", ft_atoi(argv[1]), argc);
+// }
