7bd738ded6946ac8bf6d52dc08421943796eb0bc
nastamid <nikoloz.astamidze@gmail.com>
2024-09-04 15:33:37 +0200

    fixed Atoi

--- Libft/ft_atoi.c
+++ Libft/ft_atoi.c
@@ -6,27 +6,39 @@
 /*   By: nastamid <nastamid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 23:21:52 by nastamid          #+#    #+#             */
-/*   Updated: 2024/07/23 15:11:33 by nastamid         ###   ########.fr       */
+/*   Updated: 2024/09/04 15:32:48 by nastamid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	whitespaces(char *str, int *ptr_i)
+void	whitespaces(char *str, int *ptr_i)
 {
-	int	count;
 	int	i;
 
 	i = 0;
-	count = 1;
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
 		i++;
+	*ptr_i = i;
+}
+
+int	get_sign(char *str, int *ptr_i)
+{
+	int	sign;
+	int	i;
+
+	sign = 0;
+	i = *ptr_i;
 	while (str[i] && (str[i] == '+' || str[i] == '-'))
 	{
+		if (sign != 0)
+			return (0);
 		if (str[i] == '-')
-			count *= -1;
+			sign = -1;
+		if (str[i] == '+')
+			sign = 1;
 		i++;
 	}
 	*ptr_i = i;
-	return (count);
+	return (sign);
 }
 
 int	ft_atoi(char *str)
@@ -36,20 +48,25 @@ int	ft_atoi(char *str)
 	int	i;
 
 	result = 0;
-	sign = whitespaces(str, &i);
+	sign = 0;
+	whitespaces(str, &i);
+	sign = get_sign(str, &i);
 	while (str[i] && str[i] >= '0' && str[i] <= '9')
 	{
 		result *= 10;
 		result += str[i] - 48;
 		i++;
 	}
-	result *= sign;
+	if (sign != 0)
+		result *= sign;
 	return (result);
 }
 
 // #include <stdio.h>
 
-// int	main(int argc, char *argv[])
+// int	main(void)
 // {
-// 	printf("%d argc: %d", ft_atoi(argv[1]), argc);
+// 	printf("expected: 0 result: %d ", ft_atoi("+-54"));
+// 	printf("expected: 0 result: %d ", ft_atoi("-+48"));
+// 	printf("expected: 0 result: %d ", ft_atoi("--48"));
 // }
