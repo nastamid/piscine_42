48c4abe1d247ab345b0a7ad45879c0c33a9e2405
nastamid <nikoloz.astamidze@gmail.com>
2024-09-04 16:28:20 +0200

    updated atoi and strlen, added to lower upper and strlcpy

--- Libft/ft_tolower.c
+++ Libft/ft_tolower.c
@@ -1,27 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_tolower.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nastamid <nastamid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/22 23:20:15 by nastamid          #+#    #+#             */
-/*   Updated: 2024/09/04 16:22:35 by nastamid         ###   ########.fr       */
+/*   Created: 2024/09/04 15:39:03 by nastamid          #+#    #+#             */
+/*   Updated: 2024/09/04 15:43:48 by nastamid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(char *str)
+int	ft_tolower(int c)
 {
-	int	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
+	if (c >= 'A' && c <= 'Z')
+	{
+		c += 32;
+	}
+	return (c);
 }
-
-// #include <stdio.h>
-// int main (int argc, char *argv[])
-// {
-// 	printf("%d", ft_strlen(argv[1]));
-// }
--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: nastamid <nastamid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 13:58:17 by nastamid          #+#    #+#             */
-/*   Updated: 2024/09/04 12:40:17 by nastamid         ###   ########.fr       */
+/*   Updated: 2024/09/04 16:16:39 by nastamid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,11 @@ int				ft_isalnum(int c);
 int				ft_isascii(int c);
 int				ft_isdigit(int c);
 int				ft_isprint(int c);
+int				ft_toupper(int c);
+int				ft_tolower(int c);
 void			*ft_memset(void *s, int c, int n);
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size);
 int				ft_strlen(char *str);
 int				ft_strncmp(char *s1, char *s2, unsigned int n);
+int				ft_strlcpy(char *dest, char *src, int size);
 #endif
