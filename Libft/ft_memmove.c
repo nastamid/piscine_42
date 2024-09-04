8038dc040767eef78d1f3be451e744d2ef29cf31
nastamid <nikoloz.astamidze@gmail.com>
2024-09-04 19:26:49 +0200

    simplified  memove

--- Libft/ft_memmove.c
+++ Libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: nastamid <nastamid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 19:15:22 by nastamid          #+#    #+#             */
-/*   Updated: 2024/09/04 19:24:59 by nastamid         ###   ########.fr       */
+/*   Updated: 2024/09/04 19:26:12 by nastamid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,10 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	char	*s;
 	char	*temp;
-	int		i;
 
-	s = (char *)src;
 	temp = (char *)malloc(sizeof(char *) * n);
-	i = 0;
-	while (i < (int)n)
-	{
-		temp[i] = s[i];
-		i++;
-	}
+	ft_memcpy(temp, src, n);
 	ft_memcpy(dest, temp, n);
 	return (dest);
 }
