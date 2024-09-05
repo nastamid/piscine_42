780393e9ece1271e9e02539030da9b2422402fd3
nastamid <nikoloz.astamidze@gmail.com>
2024-09-05 17:57:18 +0200

    resolved

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,11 @@
 /*   By: nastamid <nastamid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 13:58:17 by nastamid          #+#    #+#             */
+<<<<<<< HEAD
 /*   Updated: 2024/09/05 12:42:28 by nastamid         ###   ########.fr       */
+=======
+/*   Updated: 2024/09/04 22:23:44 by nastamid         ###   ########.fr       */
+>>>>>>> 517a22f (updated header)
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +33,10 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size);
 int				ft_strlen(char *str);
 int				ft_strncmp(char *s1, char *s2, unsigned int n);
 int				ft_strlcpy(char *dest, char *src, int size);
-void			*ft_memcpy(void *destination, const void *source, size_t num);
+void			*ft_memcpy(void *dest, const void *src, size_t num);
+void			*ft_memmove(void *dest, const void *src, size_t n);
+char			*ft_strchr(const char *s, int c);
+char			*ft_strrchr(const char *s, int c);
+char			*ft_strnstr(const char *big, const char *little, size_t len);
 
 #endif
