9f68c1851cd893143ee757590f592738d0af3272
nastamid <nikoloz.astamidze@gmail.com>
2024-09-06 15:51:09 +0200

    added ft_substr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,19 +6,19 @@
 /*   By: nastamid <nastamid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 19:30:46 by nastamid          #+#    #+#             */
-/*   Updated: 2024/09/04 20:06:53 by nastamid         ###   ########.fr       */
+/*   Updated: 2024/09/06 15:41:13 by nastamid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strrchr(const char *s, int c)
 {
-	const char	*result;
+	char	*result;
 
-	result = '\0';
+	result = 0;
 	while (*s)
 	{
 		if (*s == (char)c)
-			result = s;
+			result = (char *)s;
 		s++;
 	}
 	if (c != 0)
