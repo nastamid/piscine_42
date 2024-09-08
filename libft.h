4f9b1df6d80e368dd8224df6c62312a6f46be2d4
nastamid <nikoloz.astamidze@gmail.com>
2024-09-08 19:53:19 +0200

    added ft_lstclear

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: nastamid <nastamid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 13:58:17 by nastamid          #+#    #+#             */
-/*   Updated: 2024/09/08 18:53:10 by nastamid         ###   ########.fr       */
+/*   Updated: 2024/09/08 19:21:32 by nastamid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,6 +63,7 @@ int					ft_lstsize(t_list *lst);
 t_list				*ft_lstlast(t_list *lst);
 void				ft_lstadd_back(t_list **lst, t_list *new);
 void				ft_lstdelone(t_list *lst, void (*del)(void *));
+void				ft_lstclear(t_list **lst, void (*del)(void *));
 #endif
 
 /*
