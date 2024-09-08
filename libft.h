d5d95ddf1a2717f4b4383f059a683eadb3ef7120
nastamid <nikoloz.astamidze@gmail.com>
2024-09-08 19:20:34 +0200

    added lst del one

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: nastamid <nastamid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 13:58:17 by nastamid          #+#    #+#             */
-/*   Updated: 2024/09/08 18:45:00 by nastamid         ###   ########.fr       */
+/*   Updated: 2024/09/08 18:53:10 by nastamid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,6 +62,7 @@ void				ft_lstadd_front(t_list **lst, t_list *new);
 int					ft_lstsize(t_list *lst);
 t_list				*ft_lstlast(t_list *lst);
 void				ft_lstadd_back(t_list **lst, t_list *new);
+void				ft_lstdelone(t_list *lst, void (*del)(void *));
 #endif
 
 /*
