58ed19dd12c918cfcd85ea4616f69704cdcd8923
Nikoloz Astamidze <nastamid@c4r5p10.42vienna.com>
2024-11-22 16:09:07 +0100

    v2 for get next line

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,25 +6,17 @@
 /*   By: nastamid <nastamid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 17:35:13 by nastamid          #+#    #+#             */
-/*   Updated: 2024/11/20 18:16:51 by nastamid         ###   ########.fr       */
+/*   Updated: 2024/11/22 16:03:18 by nastamid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	cleanup_list(t_list **list)
+void	move_left_overs_to_new_node(t_list *last_node, t_list *node, char *buf)
 {
-	t_list	*last_node;
-	t_list	*node;
-	int		i;
-	int		j;
-	char	*buf;
+	int	i;
+	int	j;
 
-	buf = malloc(BUFFER_SIZE + 1);
-	node = malloc(sizeof(t_list));
-	if (buf == NULL || node == NULL)
-		return (0);
-	last_node = get_last_node(*list);
 	i = 0;
 	j = 0;
 	while (last_node->content[i] && last_node->content[i] != '\n')
@@ -37,26 +29,25 @@ int	cleanup_list(t_list **list)
 	buf[j] = '\0';
 	node->content = buf;
 	node->next = NULL;
-	free_memory(list, node, buf);
-	retrun(1);
 }
 
-char	*get_line(t_list *list)
+char	*combine_content_to_string(t_list *list)
 {
 	int		str_len;
-	char	*next_str;
+	char	*combined_str;
 
 	if (list == NULL)
 		return (NULL);
 	str_len = char_count_up_to_newline(list);
-	next_str = malloc(str_len + 1);
-	if (next_str == NULL)
+	combined_str = malloc(str_len + 1);
+	if (combined_str == NULL)
 		return (NULL);
-	copy_content(list, next_str);
-	return (next_str);
+	if (!copy_content(list, combined_str))
+		return (NULL);
+	return (combined_str);
 }
 
-void	append(t_list **list, char *content)
+int	append(t_list **list, char *content)
 {
 	t_list	*new_node;
 	t_list	*last_node;
@@ -64,13 +55,14 @@ void	append(t_list **list, char *content)
 	last_node = get_last_node(*list);
 	new_node = malloc(sizeof(t_list));
 	if (new_node == NULL)
-		return ;
+		return (0);
 	if (last_node == NULL)
 		*list = new_node;
 	else
 		last_node->next = new_node;
 	new_node->content = content;
 	new_node->next = NULL;
+	return (1);
 }
 
 int	create_list(t_list **list, int fd)
@@ -86,12 +78,17 @@ int	create_list(t_list **list, int fd)
 		char_read = read(fd, buf, BUFFER_SIZE);
 		if (char_read <= 0)
 		{
-			// TODO: Clean list also
+			free_list(list);
 			free(buf);
 			return (0);
 		}
 		buf[char_read] = '\0';
-		append(list, buf);
+		if (!append(list, buf))
+		{
+			free_list(list);
+			free(buf);
+			return (0);
+		}
 	}
 	return (1);
 }
@@ -99,18 +96,23 @@ int	create_list(t_list **list, int fd)
 char	*get_next_line(int fd)
 {
 	static t_list	*list = NULL;
-	char			*next_line;
+	char			*combined_str;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	create_list(&list, fd);
+	if (!create_list(&list, fd))
+	{
+		free_list(&list);
+		return (NULL);
+	}
 	if (list == NULL)
 		return (NULL);
-	next_line = get_line(list);
-	if(cleanup_list(&list))
+	combined_str = combine_content_to_string(list);
+	if (!cleanup_list(&list))
 	{
-		//TODO: Clean the list
-		return NULL;
+		free(combined_str);
+		free_list(&list);
+		return (NULL);
 	}
-	return (next_line);
+	return (combined_str);
 }
